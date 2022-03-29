69a7050a81759e6372faf422c0f22ed308098f4e
“[Zubair <[zubairbhuian@gmail.com]>
2022-03-29 10:58:36 +0600

    29.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,48 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:test_irebase2/userform.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.green),
-      home: Scaffold(
-        appBar: AppBar(title: const Text("User Informatoin")),
-        body:const UserForm(),
-      ),
-    );
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: MainBody());
   }
 }
 
-class UserForm extends StatefulWidget {
-  const UserForm({Key? key}) : super(key: key);
+class MainBody extends StatelessWidget {
+  const MainBody({Key? key}) : super(key: key);
 
-  @override
-  State<UserForm> createState() => _UserFormState();
-}
-
-class _UserFormState extends State<UserForm> {
-  bool isPasswardShow = true;
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  // textController
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      // autovalidateMode: AutovalidateMode.always,
-      child: ListView(
-        padding: const EdgeInsets.only(top: 100, left: 20, right: 20),
-        children: [
-          TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            controller: _emailController,
-            decoration: const InputDecoration(
-                border: OutlineInputBorder(), label: Text("Email")),
-            validator: (value) {
-              if (value!.isEmpty ||
-                  !RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w]{2,4}').hasMatch(value)) {
-                return "Enter Valide Email";
-              } else {
-                return null;
-              }
-            },
-          ),
-          const SizedBox(height: 20),
-          TextFormField(
-            controller: _passController,
-            obscureText: isPasswardShow,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                label: const Text("Password"),
-                suffix: InkWell(
-                  child: isPasswardShow == true
-                      ? const Icon(Icons.visibility)
-                      : const Icon(Icons.visibility_off),
-                  onTap: () {
-                    setState(() {
-                      isPasswardShow = !isPasswardShow;
-                    });
-                  },
-                )),
-            validator: (value) {
-              if (value!.isEmpty || !RegExp(r'^[w-]').hasMatch(value)) {
-                return "Enter Valid Password";
-              } else {
-                return null;
-              }
-            },
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-              onPressed: () {
-                if (formKey.currentState!.validate()) {
-                  // Process data.
-                }
-              },
-              child: const Text("Submit"))
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Hellow",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+            ),
+            ElevatedButton(onPressed: () {}, child: const Text("+"))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context, CupertinoPageRoute(builder: (_) => const UserForm()));
+        },
+        child: const Icon(Icons.arrow_right_alt),
       ),
     );
   }
--- lib/userform.dart
+++ lib/userform.dart
@@ -1,37 +1,21 @@
 import 'package:flutter/material.dart';
 
-class UserForm extends StatefulWidget {
+class UserForm extends StatelessWidget {
   const UserForm({Key? key}) : super(key: key);
 
   @override
-  State<UserForm> createState() => _UserFormState();
-}
-
-class _UserFormState extends State<UserForm> {
-  @override
   Widget build(BuildContext context) {
-    return Form(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: [
-            TextFormField(
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'Email'),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            TextFormField(
-              obscureText: true,
-              keyboardType: TextInputType.phone,
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'Password'),
-            ),
-          ],
-        ),
-      ),
+    return Scaffold(
+      appBar: AppBar(title: const Text('UserForm')),
+      body: Center(
+          child: Column(
+        children: const [
+          Text(
+            "Hellow",
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+          ),
+        ],
+      )),
     );
   }
 }
