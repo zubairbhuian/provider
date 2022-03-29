3682bfbad6c48aa545ee7cc6a02789e0a2be32e9
“[Zubair <[zubairbhuian@gmail.com]>
2022-03-29 20:08:12 +0600

    29.8

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => Counter(),
+    child: const MyApp(),
+  ));
+}
+
+//  !Provider Demo Test Class
+class Counter extends ChangeNotifier {
+  var _count = 0;
+  int get count => _count;
+  void increment() {
+    _count++;
+    notifyListeners();
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -9,25 +23,59 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SafeArea(
-          child: Scaffold(
-        appBar: AppBar(title: const Text("My App")),
-        body: DemoClass(),
-      )),
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text('You have pushed the button this many times:'),
+
+            /// Extracted as a separate widget for performance optimization.
+            /// As a separate widget, it will rebuild independently from [MyHomePage].
+            ///
+            /// This is totally optional (and rarely needed).
+            /// Similarly, we could also use [Consumer] or [Selector].
+            Count(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        key: const Key('increment_floatingActionButton'),
+
+        /// Calls `context.read` instead of `context.watch` so that it does not rebuild
+        /// when [Counter] changes.
+        onPressed: () => context.read<Counter>().increment(),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
 
-class DemoClass extends StatelessWidget {
-  const DemoClass({Key? key}) : super(key: key);
+class Count extends StatelessWidget {
+  const Count({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Center(
-        child: Container(
-          color: Colors.red,
-          child: const Text("demo"),
-        ),
-      );
+  Widget build(BuildContext context) {
+    return Text(
+
+        /// Calls `context.watch` to make [Count] rebuild when [Counter] changes.
+        '${context.watch<Counter>().count}',
+        key: const Key('counterState'),
+        style: Theme.of(context).textTheme.headline4);
+  }
 }
